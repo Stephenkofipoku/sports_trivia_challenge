0b1fd9778808296fb87a4d1880b3bca02d17f5f0
Stephenkofipoku <>
2023-09-05 21:07:51 +0000

    Add history game type javaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,57 @@ const closeModalButton = document.getElementById('close-modal');
 
 // Function to toggle the visibility of the modal overlay
 function toggleModal() {
-    modalOverlay.style.display = modalOverlay.style.display === 'none' ? 'flex' : 'none';
+  modalOverlay.style.display = modalOverlay.style.display === 'none' ? 'flex' : 'none';
 }
+
+// Add event listeners to the instruction button and close modal button
+instructionButton.addEventListener('click', toggleModal);
+closeModalButton.addEventListener('click', toggleModal);
+
+// Get the question area element
+const questionArea = document.querySelector('.question-area');
+
+// Get the score elements
+const scoreElement = document.getElementById('score');
+const incorrectElement = document.getElementById('incorrect');
+
+// Set the initial score and incorrect answer count
+let score = 0;
+let incorrect = 0;
+
+// History questions and answers
+const historyQuestions = [
+    {
+        question: "Which country won the first FIFA World Cup in 1930?",
+        options: ["Brazil", "Uruguay", "Germany"],
+        correctAnswer: 1,
+    },
+    {
+        question: "Who holds the record for the most home runs in a single MLB season?",
+        options: ["Babe Ruth", "Barry Bonds", "Hank Aaron"],
+        correctAnswer: 1,
+    },
+    {
+        question: "Which athlete won the most Olympic gold medals in history?",
+        options: ["Usain Bolt", "Michael Phelps", "Carl Lewis"],
+        correctAnswer: 1,
+    },
+    {
+        question: "Who was the first African-American to play in Major League Baseball?",
+        options: ["Jackie Robinson", "Hank Aaron", "Willie Mays"],
+        correctAnswer: 0,
+    },
+    {
+        question: "Which team won the first Super Bowl in 1967?",
+        options: ["Green Bay Packers", "Dallas Cowboys", "New England Patriots"],
+        correctAnswer: 0,
+    },
+];
 
 // Function to display a random history question
 function displayRandomHistoryQuestion() {
+    const randomQuestion = historyQuestions[Math.floor(Math.random() * historyQuestions.length)];
+
     questionArea.textContent = randomQuestion.question;
 
     for (let i = 0; i < randomQuestion.options.length; i++) {
@@ -28,129 +74,39 @@ function displayRandomHistoryQuestion() {
 // Display a random history question initially
 displayRandomHistoryQuestion();
 
-// Get the question area element
-const questionArea = document.querySelector('.question-area');
+// Function to check the selected answer
+function checkAnswer(selectedIndex) {
+    const randomQuestion = historyQuestions[Math.floor(Math.random() * historyQuestions.length)];
 
-// Get the answer box element
-const answerBox = document.getElementById('answer-box');
-
-// Get the submit answer button element
-const submitButton = document.querySelector('.btn--answer');
-
-// Get the score elements
-const scoreElement = document.getElementById('score');
-const incorrectElement = document.getElementById('incorrect');
-
-// Set the initial score and incorrect answer count
-let score = 0;
-let incorrect = 0;
-
-// History questions and answers
-const historyQuestions = [
-    {
-        question: "Which country won the first FIFA World Cup in 1930?",
-        options: ["Brazil", "Uruguay (Correct)", "Germany"],
-        correctAnswer: 1,
-    },
-    {
-        question: "Who holds the record for the most home runs in a single MLB season?",
-        options: ["Babe Ruth", "Barry Bonds (Correct)", "Hank Aaron"],
-        correctAnswer: 1,
-    },
-    {
-        question: "Which athlete won the most Olympic gold medals in history?",
-        options: ["Usain Bolt", "Michael Phelps (Correct)", "Carl Lewis"],
-        correctAnswer: 1,
-    },
-    {
-        question: "Who was the first African-American to play in Major League Baseball?",
-        options: ["Jackie Robinson (Correct)", "Hank Aaron", "Willie Mays"],
-        correctAnswer: 0,
-    },
-    {
-        question: "Which team won the first Super Bowl in 1967?",
-        options: ["Green Bay Packers (Correct)", "Dallas Cowboys", "New England Patriots"],
-        correctAnswer: 0,
-    },
-];
-
-// Wait for the DOM to finish loading before starting the game
-document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.getElementsByTagName("button");
-
-    for (let button of buttons) {
-        button.addEventListener("click", function () {
-            if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked submit!");
-            } else {
-                let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
-            }
-        });
-    }
-});
-
-function runGame() {
-    let score = 0;
-    let historyQuestions = [];
-
-    function checkAnswer(selectedIndex) {
-        if (selectedIndex === historyQuestions[0].correctIndex) {
-            incrementScore();
-        }
-        displayHistoryQuestion();
+    if (selectedIndex === randomQuestion.correctAnswer) {
+        incrementScore();
+        alert("Correct answer!");
+    } else {
+        incrementIncorrect();
+        alert("Wrong answer!");
     }
 
-    function calculateCorrectAnswer(question) {
-        return question.options[question.correctIndex];
-    }
-
-    function incrementScore() {
-        score++;
-        document.getElementById('score').textContent = score;
-    }
-
-    function displayHistoryQuestion() {
-        const questionArea = document.getElementById('question');
-        const optionsArea = document.getElementById('options');
-        const question = historyQuestions.shift();
-
-        questionArea.textContent = question.question;
-
-        optionsArea.innerHTML = '';
-        question.options.forEach((option, index) => {
-            const button = document.createElement('button');
-            button.textContent = option;
-            button.classList.add('btn');
-            button.addEventListener('click', () => checkAnswer(index));
-            optionsArea.appendChild(button);
-        });
-    }
-
-    fetch('https://api.myjson.com/bins/1gwnal')
-        .then(response => response.json())
-        .then(data => {
-            historyQuestions = data.questions;
-            displayHistoryQuestion();
-        });
+    // Display a new random history question
+    displayRandomHistoryQuestion();
 }
 
-runGame();
+// Function to increment the score
+function incrementScore() {
+    score++;
+    scoreElement.textContent = score;
+}
 
-function checkAnswer() { }
+// Function to increment the incorrect answer count
+function incrementIncorrect() {
+    incorrect++;
+    incorrectElement.textContent = incorrect;
+}
 
-function calculateCorrectAnswer() { }
+// Add event listeners to options
+questionArea.addEventListener('click', (event) => {
+    const selectedIndex = event.target.getAttribute('data-index');
 
-function incrementScore() { }
-
-function incrementWrongAnswer() { }
-
-function displayHistoryQuestion() { }
-
-function displayRulesQuestion() { }
-
-function displayRecordsQuestion() { }
-
-function displayTeamsQuestion() { }
-
-function displayQuotesQuestion() { }
+    if (selectedIndex !== null) {
+        checkAnswer(parseInt(selectedIndex));
+    }
+});
